e1694567cb052cb186cf91b62ca1e9bfbb7e0384
MarisDelunas5 <marisdelunas8@gmail.com>
2026-07-05 22:00:55 +0800

    endpoints created

--- backend/routes/feedRoutes.js
+++ backend/routes/feedRoutes.js
@@ -7,4 +7,14 @@ feedRouter.post('/create', feedController.createFeedRecord);
 
 feedRouter.get('/all', feedController.getAllFeedRecords);
 
+feedRouter.get('/batch/:batchId', feedController.getFeedByBatch);
+
+feedRouter.get('/:id', feedController.getFeedRecordById);
+
+feedRouter.put('/:id', feedController.updateFeedRecord);
+
+feedRouter.delete('/:id', feedController.deleteFeedRecord);
+
+feedRouter.get('/summary', feedController.getFeedSummary);
+
 export default feedRouter;
